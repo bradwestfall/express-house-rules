9c843d75fe87d6a19055f083dc7ac2ad7dffb040
Brad Westfall <brad@azpixels.com>
2017-05-02 12:28:46 -0700

    add tests for custom schema passed to middleware

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import request from 'supertest'
+import { Is, Schema } from 'house-rules'
 import { registerHouseRules, validQuery, validParams, validBody } from '../src'
 import houseRulesSchema from '../house-rules-schema'
 
@@ -11,6 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // Called before any other use of house-rules API
 registerHouseRules(houseRulesSchema)
+
+// Custom Schema
+const customSchema = new Schema({
+  firstName: Is.string().alpha().required().label('First Name')
+})
 
 
 /****************************************
@@ -24,15 +30,18 @@ app.get('/', (req, res) => {
 // For validQuery tests
 app.get('/valid-query-required', validQuery('r:firstName'), (req, res) => res.sendStatus(200))
 app.get('/valid-query-optional', validQuery('o:firstName'), (req, res) => res.sendStatus(200))
+app.get('/valid-query-schema', validQuery(customSchema), (req, res) => res.sendStatus(200))
 
 // For validParams tests. Note that from an express standpoint, these parameters
 // seem optional, but we're trying to test the validParams method, not express.
 app.get('/valid-params-required/:firstName?', validParams('r:firstName'), (req, res) => res.sendStatus(200))
 app.get('/valid-params-optional/:firstname?', validParams('o:firstName'), (req, res) => res.sendStatus(200))
+app.get('/valid-params-schema/:firstName', validParams(customSchema), (req, res) => res.sendStatus(200))
 
 // For validBody tests
 app.post('/valid-body-required', validBody('r:firstName'), (req, res) => res.sendStatus(200))
 app.post('/valid-body-optional', validBody('o:firstName'), (req, res) => res.sendStatus(200))
+app.post('/valid-body-schema', validBody(customSchema), (req, res) => res.sendStatus(200))
 
 
 /****************************************
@@ -46,46 +55,55 @@ describe('Express', () => {
 })
 
 describe('validQuery', () => {
-  it('should pass required validQuery method', done => {
+  it('should pass with required value', done => {
     request(app).get('/valid-query-required?firstName=abc').expect(200, done)
   })
-  it('should pass optional validQuery method', done => {
+  it('should pass with optional value', done => {
     request(app).get('/valid-query-optional').expect(200, done)
   })
-  it('should fail required validQuery method', done => {
+  it('should fail with required value', done => {
     request(app).get('/valid-query-required')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400, done)
   })
+  it('should pass custom schema', done => {
+    request(app).get('/valid-query-schema?firstName=abc').expect(200, done)
+  })
 })
 
 describe('validParams', () => {
-  it('should pass required validParams method', done => {
+  it('should pass with required value', done => {
     request(app).get('/valid-params-required/abc').expect(200, done)
   })
-  it('should pass optional validParams method', done => {
+  it('should pass with optional value', done => {
     request(app).get('/valid-params-optional').expect(200, done)
   })
-  it('should fail required validParams method', done => {
+  it('should fail with required value', done => {
     request(app).get('/valid-params-required')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400, done)
   })
+  it('should pass custom schema', done => {
+    request(app).get('/valid-params-schema/abc').expect(200, done)
+  })
 })
 
 describe('validBody', () => {
-  it('should pass required validBody method', done => {
-    request(app).post('/valid-body-required').send({firstName: 'abc'}).expect(200, done)
+  it('should pass with required value', done => {
+    request(app).post('/valid-body-required').send({ firstName: 'abc' }).expect(200, done)
   })
-  it('should pass optional validBody method', done => {
+  it('should pass with optional value', done => {
     request(app).post('/valid-body-optional').expect(200, done)
   })
-  it('should fail required validBody method', done => {
+  it('should fail with required value', done => {
     request(app).post('/valid-body-required')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(400, done)
+  })
+  it('should pass custom schema', done => {
+    request(app).post('/valid-body-schema').send({ firstName: 'abc' }).expect(200, done)
   })
 })
